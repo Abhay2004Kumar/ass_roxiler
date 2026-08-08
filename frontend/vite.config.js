3382421a79523113c241f421d8889c9de3d9501b
Abhay2004Kumar <abs220502@gmail.com>
2026-08-08 13:22:42 +0530

    removed hardcoding of backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      // Forward all /api/* calls to the Express backend
-      '/api': {
-        target: 'https://ass-roxiler.onrender.com',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        // Forward all /api/* calls to the Express backend if hitting proxy
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
